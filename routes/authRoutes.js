68e755dcaa0eb4d579c6a07ec22f45dc19f012d0
Tifar <istighfar.amal@gmail.com>
2024-02-28 04:14:59 +0700

    auth before validation logic

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,44 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const AuthController = require("../controllers/AuthController");
-const PasswordResetController = require("../controllers/PasswordResetController");
-
-const { rateLimit } = require("express-rate-limit");
-
-const UserRateLimitStore = require("../utils/UserRateLimitStore");
-
-const windowMs = 15 * 60 * 1000;
-
-const userRateLimitStoreInstance = new UserRateLimitStore(windowMs);
-
-const userLoginLimiter = rateLimit({
-  store: userRateLimitStoreInstance,
-  windowMs: windowMs,
-  max: 5,
-  skipSuccessfulRequests: true,
-  message: {
-    error: "Too many failed login attempts, please try again after 15 minutes.",
-  },
-  keyGenerator: (req) => {
-    const key = req.body.username;
-    console.log(`Generating key for rate limiter: ${key}`);
-    return key;
-  },
-});
-
-router.post("/login", userLoginLimiter, AuthController.login);
-
-router.post("/register", AuthController.register);
-router.get("/verify-email/:token", AuthController.verifyEmail);
+const authController = require("../controllers/authController");
+const authValidation = require("../validations/authValidation");
 
 router.post(
-  "/request-password-reset",
-  PasswordResetController.requestPasswordReset
+  "/register",
+  authValidation.registerValidation,
+  authController.register
 );
 
-router.post(
-  "/reset-password/:resetToken",
-  PasswordResetController.resetPassword
+router.post("/login", authValidation.loginValidation, authController.login);
+
+router.put(
+  "/setUsername",
+  authValidation.setUsernameValidation,
+  authController.setUsername
 );
 
 module.exports = router;
