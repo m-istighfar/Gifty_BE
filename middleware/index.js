aa4956054553552b3f194d2f8196e9ed368c9eba
Tifar <istighfar.amal@gmail.com>
2024-03-09 17:23:18 +0700

    allow all cors

--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,9 @@
 const applyBodyParser = require("./bodyParser");
 const applyHelmet = require("./helmet");
-// const applyCors = require("./cors");
 const applyPermissionPolicy = require("./setPermissionPolicy");
 
 module.exports = (app) => {
   applyHelmet(app);
-  // applyCors(app);
   applyBodyParser(app);
   applyPermissionPolicy(app);
 };
